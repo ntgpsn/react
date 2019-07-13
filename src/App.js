433ae76096d397091a1fe6cfd54c0d1fca64d0fc
ntgpsn <137355523@qq.com>
2019-07-13 18:47:01 +0800

    first comit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Button text={'hhhasdasdh'}></Button>
+    </div>
+  );
+}
+
+export default App;
